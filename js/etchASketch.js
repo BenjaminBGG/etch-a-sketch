32c8caba9a0703aff889e537919a814676cbf134
BenjaminBGG <54398178+BenjaminBGG@users.noreply.github.com>
2022-12-18 06:18:32 -0700

    Added folders & css boilerplate

---
+++ js/etchASketch.js
@@ -0,0 +1,37 @@
+            //  --- GLOBAL VARIABLES --- //            
+var penColor = '';
+var gridSize = 16;
+            //  --- CREATE GRID ---  //
+
+    // Call createGrid()
+
+            //  --- FUNCTIONS ---  //
+
+        // - GET NEW COLOR - //
+function getColor () {
+
+};
+
+function getSize() {
+
+};
+
+function createGrid() {
+
+};
+
+function createCell() {
+
+};
+
+function resetGrid() {
+
+};
+
+            //  --- EVENT LISTENERS ---  //
+
+        // - MOUSEOVER - //
+
+    // Update grid color on mouseover
+
+
