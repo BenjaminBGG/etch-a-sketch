4049b534297a81ae2e67de81b246ef51d87a0a51
BenjaminBGG <54398178+BenjaminBGG@users.noreply.github.com>
2022-12-19 15:47:08 -0700

    Completed working model;
    Todo:
        Add CSS for buttons
        Add Option to change background color
        Force VH to full

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -1,37 +1,81 @@
+            
+            
             //  --- GLOBAL VARIABLES --- //            
-var penColor = '';
-var gridSize = 16;
+var penColor = 'var(--black)';
+var color = 'var(--black)';
+let click = true;
             //  --- CREATE GRID ---  //
 
-    // Call createGrid()
+createGrid(16);
 
             //  --- FUNCTIONS ---  //
 
         // - GET NEW COLOR - //
-function getColor () {
 
-};
+function createGrid(size){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    let amount = math.square(size);
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', setCellColor);
+        square.style.backgroundColor = 'var(--white)';
+        board.insertAdjacentElement('beforeend', square);
+    }
+}
 
-function getSize() {
+function setCellColor () {
+    if(click){    
+        if(penColor === 'random'){
+            this.style.backgroundColor = getRandomColor();
+        } else {
+            this.style.backgroundColor = penColor;
+        }
 
-};
+    }
+}
 
-function createGrid() {
+function setColor(choice) {
+    penColor = choice;
+    console.log(choice);
+}
 
-};
+function getRandomColor() {
+    return '#'+(Math.random()*0xFFFFFF<<0).toString(16); 
+  }
 
-function createCell() {
-
+function setSize(size) {
+    if(size >= 2 && size <= 100){
+        document.querySelector('.error').textContent = '';
+        createGrid(size);
+    }
+    else {
+        document.querySelector('.error').textContent = 'Input Must Be Between 2 & 100';
+    }
 };
 
 function resetGrid() {
-
+    createGrid(16);
+    penColor = 'var(--black)';
+    click = true;
+    document.querySelector('.mode').textContent = 'Mode: Coloring';
 };
 
             //  --- EVENT LISTENERS ---  //
 
-        // - MOUSEOVER - //
+// (de)activate pen on click
 
-    // Update grid color on mouseover
-
+document.querySelector('body').addEventListener('click', (e) => { 
+    if (e.target.tagName != 'BUTTON'){
+        click = !click;
+        if(click) { 
+            document.querySelector('.mode').textContent = 'Mode: Coloring';
+        } else {
+            document.querySelector('.mode').textContent = 'Mode: Not Coloring';
+        }
+    }
+});
 
